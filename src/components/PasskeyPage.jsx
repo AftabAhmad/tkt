a211adf7e39997614370882631ec0abf9db33914
Aftab Ahmad <i.aftabawan@gmail.com>
2026-06-18 21:02:09 +0500

    adding iframe

--- src/components/PasskeyPage.jsx
+++ src/components/PasskeyPage.jsx
@@ -56,7 +56,7 @@ export default function PasskeyPage() {
 	if (verified) {
 		return (
 			<iframe
-				src="https://api.tokenhousetoken.com"
+				src="https://classy-cdkbd7wm.peachweb.site/"
 				style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', border: 'none', zIndex: 9999 }}
 				title="TokenHouseToken"
 			/>
